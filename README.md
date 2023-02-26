3c86f27f7eb079848e5c18ff91cfcdfa62161b00
Sagar <93700292+syntacticsagar@users.noreply.github.com>
2023-02-26 21:37:19 +0000

    Adds basic nix build inputs for the project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # despairallel
+
+Using this repo to dive into the frustratingly wonderful world of distributed systems. We will try to solve the challenges in https://fly.io/dist-sys/ and see how far we get!
---
+++ default.nix
@@ -0,0 +1,10 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { overlays = [ ] ; config = {}; };
+in pkgs.mkShell {
+  buildInputs = [
+    pkgs.jdk17_headless
+    pkgs.graphviz
+    pkgs.gnuplot
+  ];
+}
